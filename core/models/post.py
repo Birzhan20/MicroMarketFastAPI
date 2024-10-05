9d03d34c16160d3fdcebd25144fbcc92db54bc01
BIRZHAN <birzhan981@gmail.com>
2024-10-05 13:59:43 +0500

    create relationships between user and post

--- core/models/post.py
+++ core/models/post.py
@@ -1,7 +1,14 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import (
+        User,
+    )  #  срабатывает только если происходит проверка кода, н вызывает циклическую потлю импортов
 
 
 class Post(Base):
@@ -11,7 +18,9 @@ class Post(Base):
         default="",
         server_default="",
     )
-
     user_id: Mapped[str] = mapped_column(
         ForeignKey("users.id"),
     )
+    user: Mapped["User"] = relationship(
+        back_populates="posts"
+    )  #  ссылается на юзера через  пост
