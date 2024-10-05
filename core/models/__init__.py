f95c68d6e4923b690939f60541901850d8289ebe
BIRZHAN <birzhan981@gmail.com>
2024-10-05 17:30:50 +0500

    one to many

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -6,6 +6,7 @@ __all__ = (
     "User",
     "Post",
     "Profile",
+    "Order",
 )
 
 from .base import Base
@@ -14,3 +15,4 @@ from .db_helper import db_helper, DatabaseHelper
 from .user import User
 from .post import Post
 from .profile import Profile
+from .order import Order
